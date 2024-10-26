1d1571d9abd48f1bb95b05cc19fcdc7219a40895
Noemi Giustini <giustini.1933541@studenti.uniroma1.it>
2024-10-26 17:28:09 +0200

    added calendar in registration form and back button

--- app/src/main/java/com/safetyhat/macc/RegistrationActivity.kt
+++ app/src/main/java/com/safetyhat/macc/RegistrationActivity.kt
@@ -16,6 +16,10 @@ import org.json.JSONObject
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Locale
+import android.app.DatePickerDialog
+import android.content.Intent
+import java.util.Calendar
+import android.widget.ImageView
 
 
 class RegistrationActivity : AppCompatActivity() {
@@ -25,13 +29,25 @@ class RegistrationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        val backButton = findViewById<ImageView>(R.id.back_icon)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Chiude la RegistrationActivity
+        }
+
         val firstNameField = findViewById<EditText>(R.id.first_name_registration_field)
         val lastNameField = findViewById<EditText>(R.id.last_name_registration_field)
-        val birthdateField = findViewById<EditText>(R.id.birthdate_registration_field)
+        //val birthdateField = findViewById<EditText>(R.id.birthdate_registration_field)
         val phoneField = findViewById<EditText>(R.id.phone_registration_field)
         val cfField = findViewById<EditText>(R.id.cf_registration_field)
         val passwordField = findViewById<EditText>(R.id.password_registration_field)
         val submitButton = findViewById<Button>(R.id.submit_registration_button)
+
+        val birthdateField = findViewById<EditText>(R.id.birthdate_registration_field)
+        birthdateField.setOnClickListener {
+            showDatePickerDialog(birthdateField)
+        }
 
         submitButton.setOnClickListener {
             val firstName = firstNameField.text.toString()
@@ -43,12 +59,32 @@ class RegistrationActivity : AppCompatActivity() {
 
             if (firstName.isNotEmpty() && lastName.isNotEmpty() && birthdate.isNotEmpty() &&
                 phone.isNotEmpty() && cf.isNotEmpty() && password.isNotEmpty()) {
-                // Esegui la registrazione dell'utente
                 registerWorker(firstName, lastName, birthdate, phone, cf, password)
             } else {
                 Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun showDatePickerDialog(birthdateField: EditText) {
+        val calendar = Calendar.getInstance()
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            R.style.CustomDatePickerDialog,
+            { _, year, month, dayOfMonth ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
+
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                birthdateField.setText(dateFormat.format(selectedDate.time))
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.show()
     }
 
     private fun registerWorker(
